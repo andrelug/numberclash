ea983a38b8dbd2bb7a15ab9f575085d08f3a0eec
Andrelug <pamasway@gmail.com>
2014-03-18 04:11:13 -0300

    First production deploy

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '471413439653703', // your App ID
 		'clientSecret' 	: 'b7c033b4c7877410afc5e25493b299ae', // your App Secret
-		'callbackURL' 	: 'http://localhost:26676/auth/facebook/callback'
+		'callbackURL' 	: 'http://www.numbersclash.com/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'VW9Qu0dlq9cKE5TlkU3joQ',
 		'consumerSecret' 	: '8Hg3uADYMHSCouhe9W8YmtN3QWf6q1eXf1xBJbUnf8c',
-		'callbackURL' 		: 'http://localhost:26676/auth/twitter/callback'
+		'callbackURL' 		: 'http://www.numbersclash.com/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '886140459979-r26dp1t5it9a8oc6cjevhe0p4mrvr2te.apps.googleusercontent.com',
 		'clientSecret' 	: 'nKSXebFZEen0mmKB8WuuIXuK',
-		'callbackURL' 	: 'http://localhost:26676/auth/google/callback'
+		'callbackURL' 	: 'http://www.numbersclash.com/auth/google/callback'
 	}
 
 };
