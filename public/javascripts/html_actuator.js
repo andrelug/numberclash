c714d328f13066cbaf354ebd5782ebd9ec0163e1
Andrelug <pamasway@gmail.com>
2014-03-24 06:29:23 -0300

    removed custom analytics

--- public/javascripts/html_actuator.js
+++ public/javascripts/html_actuator.js
@@ -147,18 +147,6 @@ HTMLActuator.prototype.message = function (won, score, bestScore) {
         }
     });
 
-    var dimensionValue = logged;
-    ga('set','dimension1', dimensionValue);
-
-    var metricValue = score;
-    ga('set', 'metric1', metricValue);
-
-    ga('send', 'event', 'category', 'action', {
-      'metric18': 8000
-    });
-
-    ga('send', 'event', 'game', 'finish', { 'metric1': metricValue, 'dimension1': dimensionValue });
-
     Stop();  
 
   this.messageContainer.classList.add(type);
