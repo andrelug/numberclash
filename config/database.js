c4dda3ecb755901f02446f2ef1afdc50aedc8722
Andrelug <pamasway@gmail.com>
2014-03-27 01:15:43 -0300

    db fix

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ var express = require('express'),
 
 if('development' == app.get('env')) {
     module.exports = {
-        'url': 'mongodb://localhost/numberclash',
-        'url2': 'mongodb://localhost/numberclashsessions'
+        'url': 'mongodb://NumberClashMongo:kqqSHDyxCDyzOWVYaO50SksNu4Q8EoUd2Ykdhnc_7UE-@ds031088.mongolab.com:31088/NumberClashMongo',
+        'url2': 'mongodb://ncsessions:i127C0MLpkYCvPwMAZ.SCBuQy_cINeKuxPtPOG1z35c-@ds035747.mongolab.com:35747/ncsessions'
     }
 }else{
     module.exports = {
