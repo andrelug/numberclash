ec73fd584ea4db39d20f22b661198ca951ccd0a5
Andrelug <pamasway@gmail.com>
2014-03-29 17:56:12 -0300

    mongolab uris

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ if('development' == app.get('env')) {
     }
 }else{
     module.exports = {
-        'url': 'mongodb://NumberClashMongo:kqqSHDyxCDyzOWVYaO50SksNu4Q8EoUd2Ykdhnc_7UE-@ds031088.mongolab.com:31088/NumberClashMongo',
-        'url2': 'mongodb://ncsessions:i127C0MLpkYCvPwMAZ.SCBuQy_cINeKuxPtPOG1z35c-@ds035747.mongolab.com:35747/ncsessions'
+        'url': process.env.CUSTOMCONNSTR_MONGOLAB_URI,
+        'url2': process.env.CUSTOMCONNSTR_MONGOLAB_URIA
     }
 }
